f51aaf7611fc648bbea2fa9ab2ff1a378f1474a7
BenNagurskiUU <nagurski-b@ulster.ac.uk>
2025-10-06 15:01:11 +0100

    Added a constructor to the book class

--- Week3Library/Program.cs
+++ Week3Library/Program.cs
@@ -1,36 +1,34 @@
-﻿class Book
+﻿namespace Week3Library
 {
-    string Title;
-    string Author;
-    string ISBN;
-
-    void DisplayInfo()
+    internal class Book
     {
-        Console.WriteLine($"Book title: {Title}");
-        Console.WriteLine($"Book author: {Author}");
-        Console.WriteLine($"Book ISBN: {ISBN}");
-        Console.WriteLine();
-    }
-    static void Main(string[] args)
-    {
-        // create new instance of the Book class
-        Book book = new Book();
+        string Title;
+        string Author;
+        string ISBN;
 
-        // info for one book in the library
-        book.Title = "C# for beginners";
-        book.Author = "BillGates";
-        book.ISBN = "12345678";
 
-        // create another book
-        Book book1 = new Book();
-        book1.Title = "C# Methods and classes";
-        book1.Author = "Microsoft";
-        book1.ISBN = "55667778";
+        //constructor
+        public Book (string bookTitle, string bookAuthor, string bookISBN)
+        {
+            Title = bookTitle;
+            Author = bookAuthor;
+            ISBN = bookISBN;
+        }
+        void DisplayInfo()
+        {
+            Console.WriteLine($"Book title: {Title}");
+            Console.WriteLine($"Book author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            // create new instance of the Book class
+            Book book = new Book("C# for beginners", "Bill Gates", "1234567");
 
-        // output book info to console
-        book.DisplayInfo();
-
-        // output book1 info to console
-        book1.DisplayInfo();
+            // display book info
+            book.DisplayInfo();
+        }
     }
 }
+
