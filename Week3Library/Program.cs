1bcd568cf8a0b202b448585b821a4b1bcd7cc58b
BenNagurskiUU <nagurski-b@ulster.ac.uk>
2025-10-06 14:52:34 +0100

    Created book class

--- Week3Library/Program.cs
+++ Week3Library/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+
+    void DisplayInfo()
+    {
+        Console.WriteLine($"Book title: {Title}");
+        Console.WriteLine($"Book author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
+        Console.WriteLine();
+    }
+    static void Main(string[] args)
+    {
+        // create new instance of the Book class
+        Book book = new Book();
+
+        // info for one book in the library
+        book.Title = "C# for beginners";
+        book.Author = "BillGates";
+        book.ISBN = "12345678";
+
+        // create another book
+        Book book1 = new Book();
+        book1.Title = "C# Methods and classes";
+        book1.Author = "Microsoft";
+        book1.ISBN = "55667778";
+
+        // output book info to console
+        book.DisplayInfo();
+
+        // output book1 info to console
+        book1.DisplayInfo();
+    }
+}
